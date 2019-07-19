fe0ade6be23cbeb42ff5e14952baf49059d9e3fc
Alex Munro <alexmunro91@gmail.com>
2019-07-20 00:01:32 +0100

    Solution for Day 16, part two

--- src/challenges/Day16.kt
+++ src/challenges/Day16.kt
@@ -5,41 +5,53 @@ import java.lang.IndexOutOfBoundsException
 
 private val input = importDataStr(16)
 
+data class DataSample(val instruction: Instruction, val r1: Array<Int>, val r2: Array<Int>)
+data class Instruction(val opCode: Int, val a: Int, val b: Int, val c: Int)
 
-data class DataSample(val a: Int, val b: Int, val c: Int, val r1: Array<Int>, val r2: Array<Int>)
+fun getArray(line: String): Array<Int> {
+    return Regex("""\[((?:\d+, )*\d+)\]""")
+        .find(line)!!.groupValues[1]
+        .split(", ")
+        .map{ it.toInt() }
+        .toTypedArray()
+}
 
 private val dataSamples: List<DataSample> = {
     var i = 0
     val samples = mutableListOf<DataSample>()
     
     while(input[i].startsWith("Before: ")){
-        
-        fun getArray(line: String): Array<Int> {
-            return Regex("""\[((?:\d+, )*\d+)\]""")
-                .find(line)!!.groupValues[1]
-                .split(", ")    
-                .map{ it.toInt() }
-                .toTypedArray()
-        }
-        
         val r1 = getArray(input[i])
         i++
         
-        val (_, a, b, c) = input[i].split(" ").map{ it.toInt() }
+        val (opCode, a, b, c) = input[i].split(" ").map{ it.toInt() }
         i++
         
         val r2 = getArray(input[i])
         i += 2
         
-        samples.add(DataSample(a, b, c, r1, r2))
+        samples.add(DataSample(Instruction(opCode, a, b, c), r1, r2))
     }
     
     samples
 }()
 
-typealias OpCode = (Int, Int, Int, Array<Int>) -> Array<Int>
+private val instructions: List<Instruction> = {
+    var i = 0
+    while(input[i].startsWith("Before: ")){
+        i += 4
+    }
+    i += 2
+    
+    input.takeLast(input.size - i).map{
+        val (opCode, a, b, c) = it.split(" ").map{ it.toInt() }
+        Instruction(opCode, a, b, c)
+    }
+}()
 
-val opCodes: Map<String, OpCode> = mapOf(
+typealias Operation = (Int, Int, Int, Array<Int>) -> Array<Int>
+
+val operations: Map<String, Operation> = mapOf(
 
     // Addition
     "addr" to { a, b, c, r -> r.clone().also { it[c] = r[a] + r[b] } },
@@ -73,17 +85,56 @@ val opCodes: Map<String, OpCode> = mapOf(
     "eqrr" to { a, b, c, r -> r.clone().also{ it[c] = if (r[a] == r[b]) 1 else 0 } }
 )
 
+val mapOpcodes: Map<Int, Operation> = {
+    val remainingOperations = operations.toMutableMap()
+    var remainingSamples = dataSamples
+    val opcodeMap = mutableMapOf<Int, Operation>()
+    
+    while (remainingOperations.isNotEmpty()) {
+        
+        val assignableOperations: Map<Int, Map<String, Operation>> = remainingSamples.map{ (i, r1, r2) ->
+            i.opCode to remainingOperations.filter {
+                try { 
+                    it.value.invoke(i.a, i.b, i.c, r1).contentEquals(r2)
+                } catch (e: IndexOutOfBoundsException) {
+                    false
+                }
+            }
+        }.filter { it.second.size == 1 }.toMap()
+
+        if (assignableOperations.isEmpty())
+            throw Exception("Unable to assign all opcodes")
+
+        assignableOperations.forEach { op ->
+            opcodeMap[op.key] = op.value.values.first()
+            remainingOperations.remove(op.value.keys.first())
+            remainingSamples = remainingSamples.filterNot{ it.instruction.opCode == op.key }
+        }
+    }
+    
+    opcodeMap
+}()
+
+fun execute(initialRegisters: Array<Int>): Array<Int>{
+    var registers = initialRegisters
+    instructions.forEach {
+        val operation = mapOpcodes[it.opCode] ?: error("Operation not found")
+        registers = operation.invoke(it.a, it.b, it.c, registers)
+    }
+    return registers
+}
+
 private fun first(): Int {
-    return dataSamples.count{ (a, b, c, r1, r2) -> 
-        opCodes.count { 
-            try { it.value.invoke(a, b, c, r1).contentEquals(r2) } 
+    return dataSamples.count{ (i, r1, r2) -> 
+        operations.count { 
+            try { it.value.invoke(i.a, i.b, i.c, r1).contentEquals(r2) } 
             catch (e: IndexOutOfBoundsException) { false }
         } >= 3
     }
 }
 
-private fun second() {
-
+private fun second(): Int {
+    return execute(arrayOf(0, 0, 0, 0))[0]
 }
 
 fun main(args: Array<String>) {
